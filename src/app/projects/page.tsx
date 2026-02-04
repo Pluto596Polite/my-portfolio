d9cfe762e50bb02584cfd321f0b3d9723a6b491f
Pluto596Polite <avanwyk191@gmail.com>
2026-02-04 18:32:44 +0200

    feat: Add initial projects page with basic layout and navigation.

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../Components/Navbar";
+import Link from "next/link";
+export default function Projects() {
+    return (
+        <div className="container mx-auto px-5">
+            <Navbar />
+            <h1 className="text-2xl font-bold font-serif">Projects</h1>
+            <p className="text-lg font-serif">Here are some of my projects</p>
+        </div>
+    );
+}
